fed96bbd70760ce8c48294195b82fc84f19bc61e
trinity-rank <nebojsa@trinityrank.com>
2021-10-26 15:48:00 +0200

    default api token fix

--- src/GeoLocationOperater.php
+++ src/GeoLocationOperater.php
@@ -7,19 +7,23 @@ use App\Models\Operater;
 
 class GeoLocationOperater
 {
+    /*
+    Test IP addresses
+    uk - 185.44.76.168
+    it - 185.217.71.4
+    fr - 143.244.57.123
+    */
+
+
     // This is default admin token
-    public static function api_call($api_token = "d1FfPSYIUxQIuPcQa6k3UJ8LvCnDVkLSYvAaPsSKTb44A6smrn2hz77kE7H85g8PhzmvHYpTAlZ0vxGw")
+    public static function api_call($api_token)
     {
-        $curl = curl_init();
+        // User IP address
         $user_ip = Request::ip();
+        // Default "admin" api token
+        $api_token = $api_token ?? "d1FfPSYIUxQIuPcQa6k3UJ8LvCnDVkLSYvAaPsSKTb44A6smrn2hz77kE7H85g8PhzmvHYpTAlZ0vxGw";
 
-        /*
-        Test IP addresses
-        uk - 185.44.76.168
-        it - 185.217.71.4
-        fr - 143.244.57.123
-        */
-
+        $curl = curl_init();
         curl_setopt_array($curl, array(
             // CURLOPT_URL => 'https://geo-location.test/api/location', // localhost project
             CURLOPT_URL => 'http://143.198.178.43/api/location',
@@ -42,12 +46,17 @@ class GeoLocationOperater
         $response = curl_exec($curl);
     
         curl_close($curl);
+
+        echo $api_token;
+        die;
         return $response;
     }
 
 
-    public static function list($decorators, $api_token = "")
+    public static function list($decorators, $api_token = null)
     {
+        
+
         // List of operaters ID
         $tableElements = json_decode($decorators);
         
